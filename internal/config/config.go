f994ad5515094fae599400e180dc6aab4ed17f1b
AbdulBari <bay7amov@gmail.com>
2025-01-29 22:47:07 +0300

    added layout reddis

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+type Config struct {
+	Env         string `yaml:"env"`
+	StoragePath string `yaml:"storage_path"`
+	HTTPServer  struct {
+		Address     string `yaml:"address"`
+		Timeout     string `yaml:"timeout"`
+		IdleTimeout string `yaml:"idle_timeout"`
+		User        string `yaml:"user"`
+		Password    string `yaml:"password"`
+	}
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Dbname   string `yaml:"dbname"`
+	}
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
